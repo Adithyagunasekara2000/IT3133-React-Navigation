e1d0ef60dd8de75cfd4bbb0fc5998c11af13a0ec
Adithyagunasekara2000 <139615785+Adithyagunasekara2000@users.noreply.github.com>
2024-11-29 08:21:48 +0000

    components

---
+++ reactnavi1/src/components/AboutUs.js
@@ -0,0 +1,8 @@
+export default function AboutUs(){
+    return(
+        <div>
+            <h1>Welcome to about page</h1>
+        </div>
+
+    )
+}
---
+++ reactnavi1/src/components/ContactUs.js
@@ -0,0 +1,7 @@
+export default function ContactUs(){
+    return(
+        <div>
+        <h1>Welcom to contact us page</h1>
+        </div>
+    )
+}
