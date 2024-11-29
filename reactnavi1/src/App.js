eb1a7e94b451d7ba9333fea39af8119ba0c3575b
Adithyagunasekara2000 <139615785+Adithyagunasekara2000@users.noreply.github.com>
2024-11-29 09:29:32 +0000

    link

--- reactnavi1/src/App.js
+++ reactnavi1/src/App.js
@@ -1,9 +1,25 @@
-import {BrowserRouter as Router,Routes,Route,Link,NavLink} from ''
+import {BrowserRouter as Router,Routes,Route,Link,NavLink} from 'react-router-dom';
+import Home from './components/Home'
+import ContactUs from './components/ContactUs'
+import AboutUs from './components/AboutUs'
 
 function App(){
   return(
 <>
-
+<Router>
+  <nav>
+    <ul>
+      <li><Link to="/home">Home</Link></li>
+      <li><Link to="/about">AboutUs</Link></li>
+      <li><Link to="/contact">ContactUs</Link></li>
+    </ul>
+  </nav>
+  <Routes>
+    <Route path='/home' element={<Home/>}/>
+    <Route path='/contact' element={<ContactUs/>}/>
+    <Route path='/about' element={<AboutUs/>}/>
+  </Routes>
+</Router>
 </>
   )
 }
