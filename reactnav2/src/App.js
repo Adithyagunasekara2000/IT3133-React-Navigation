c28a1028f26729f0d32cf785f820c7fa36f4c0de
Adithyagunasekara2000 <139615785+Adithyagunasekara2000@users.noreply.github.com>
2024-11-29 11:29:24 +0000

    login

--- reactnav2/src/App.js
+++ reactnav2/src/App.js
@@ -1,5 +1,7 @@
 import {BrowserRouter as Router,Routes,Route,Link,NavLink} from 'react-router-dom'
 import DashBoard from './components/DashBoard';
+import Login from './components/Login'
+
 function App(){
   return(
     <>
